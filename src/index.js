b57ef4476c8f3804f127241bd10d6d55e3bca6cb
Alan <alanstvn420@gmail.com>
2024-06-28 23:21:03 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-const express = require("express");
-const app = express();
+// Importa la instancia de la aplicación express desde el archivo "server.js"
+import app from "./server.js";
+// Importar la función connection()
+import connection from "./database.js"; // Importa la función 'connection' desde el archivo "database.js" para establecer la conexión con la base de datos
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// Haciendo uso de la función connection()
+connection(); // Llama a la función 'connection' para establecer la conexión con la base de datos
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+// Escucha las conexiones entrantes en el puerto configurado en la aplicación express
+app.listen(app.get("port"), () => {
+  console.log(`Server connected to port: ${process.env.port}`); // Imprime un mensaje indicando la dirección del servidor
+});
+
+
