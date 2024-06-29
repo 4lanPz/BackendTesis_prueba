fa330676d59e51a162d6420f149f7fba3eda067c
Alan <alanstvn420@gmail.com>
2024-06-28 23:16:55 -0500

    cambio archivo vercel

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
-// Importa la instancia de la aplicación express desde el archivo "server.js"
-import app from "./server.js";
+// // Importa la instancia de la aplicación express desde el archivo "server.js"
+// import app from "./server.js";
 // Importar la función connection()
 import connection from "./database.js"; // Importa la función 'connection' desde el archivo "database.js" para establecer la conexión con la base de datos
 
-
 // Haciendo uso de la función connection()
 connection(); // Llama a la función 'connection' para establecer la conexión con la base de datos
-
 
 // Escucha las conexiones entrantes en el puerto configurado en la aplicación express
 app.listen(app.get("port"), () => {
   console.log(`Server connected to port: ${process.env.port}`); // Imprime un mensaje indicando la dirección del servidor
 });
 
+// Requerir los módulos
+import express from "express"; // Importa el módulo 'express' para crear y configurar el servidor
+import dotenv from "dotenv"; // Importa el módulo 'dotenv' para cargar variables de entorno desde un archivo '.env'
+import cors from "cors"; // Importa el módulo 'cors' para permitir solicitudes de diferentes orígenes en el servidor
 
+// Importar la variable routerTecnicos
+import routerTecnicos from "./routers/tecnico_routes.js"; // Importa el enrutador definido para las rutas relacionadas con tecnicos
+// Importar la variable routerPacientes
+import routerClientes from "./routers/cliente_routes.js"; // Importa el enrutador definido para las rutas relacionadas con clientes
+// Importar la variable routerTratamientos
+import routerOrdentrabajo from "./routers/ordentrabajo_routes.js"; // Importa el enrutador definido para las rutas relacionadas con ordenes
+import routerProformas from "./routers/proforma_routes.js"; // Importa el enrutador definido para las rutas relacionadas con proformas
+
+// Inicializaciones
+const app = express(); // Crea una instancia de la aplicación express
+dotenv.config(); // Configura dotenv para cargar las variables de entorno desde el archivo .env
+
+// Configuraciones
+app.set("port", process.env.port || 3000); // Configura el puerto en el que se ejecutará el servidor, tomando el valor de una variable de entorno 'port' si está definida, de lo contrario usa el puerto 3000
+app.use(cors()); // Usa el middleware cors para permitir solicitudes de diferentes orígenes en el servidor
+
+// Middlewares
+app.use(express.json()); // Usa el middleware integrado de express para analizar las solicitudes entrantes con formato JSON
+
+// Rutas
+app.use("/api", routerTecnicos); // Usa el enrutador de veterinarios en la ruta '/api'
+app.use("/api", routerClientes); // Usa el enrutador de pacientes en la ruta '/api'
+app.use("/api", routerOrdentrabajo); // Usa el enrutador de tratamientos en la ruta '/api'
+app.use("/api", routerProformas); // Usa el enrutador de tratamientos en la ruta '/api'
+
+// Manejo de una ruta que no sea encontrada
+app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404")); // Maneja las solicitudes a rutas no encontradas y responde con un mensaje de error 404
+
+// Exportar la instancia de express por medio de app
+export default app; // Exporta la instancia de la aplicación express para que pueda ser utilizada en otros archivos del proyecto
